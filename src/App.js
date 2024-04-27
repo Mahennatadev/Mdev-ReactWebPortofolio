a2e6945db29a350904414bb50196d971e5112228
Mahennatadev <121371000+Mahennatadev@users.noreply.github.com>
2024-04-27 20:29:59 +0700

    make(Home-About-Skills)
    make some features

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { useState } from 'react';
 import "./App.css";
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
+import Service from './components/service/Service';
+import Contact from './components/contact/Contact';
+import Portofolio from './components/portofolio/Portofolio';
 
 const App = () => {
   return (
@@ -11,6 +16,11 @@ const App = () => {
 
     <main className='main'>
       <Home />
+      <About />
+      <Skills />
+      <Service />
+      <Portofolio />
+      <Contact />
     </main>
     </>
   )
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <section className="contact section" id="contact">
+      <h2 className="section__title">Contact</h2>
+      <span className="section__subtitle">Service is in progress</span>
+    </section>
+  )
+}
+
+export default Contact
