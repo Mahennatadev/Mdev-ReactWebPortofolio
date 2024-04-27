86bba7dd1bb649a218550e2cdd57a91f9f411ae7
Mahennatadev <121371000+Mahennatadev@users.noreply.github.com>
2024-04-27 16:55:16 +0700

    first commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://www.instagram.com" className="home__social-icon" target="_blank">
+        <i className="uil uil-instagram"></i>
+      </a>
+
+      <a href="https://github.com" className="home__social-icon" target="_blank">
+        <i className="uil uil-github-alt"></i>
+      </a>
+
+      <a href="https://www.linkedin.com/" className="home__social-icon" target="_blank">
+      <i className="uil uil-linkedin"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
