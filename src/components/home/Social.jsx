2e08c0bf2e01527ee31ab8571621cc17f7abb319
Mahennatadev <121371000+Mahennatadev@users.noreply.github.com>
2024-06-28 13:53:13 +0700

    update package.json

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,16 +3,16 @@ import React from "react";
 const Social = () => {
   return (
     <div className="home__social">
-      <a href="https://www.instagram.com" className="home__social-icon" target="_blank">
-        <i className="uil uil-instagram"></i>
+      <a href="https://www.linkedin.com/in/wahyunatamahendra" className="home__social-icon" target="_blank" rel="noopener noreferrer">
+        <i className="uil uil-linkedin"></i>
       </a>
 
-      <a href="https://github.com" className="home__social-icon" target="_blank">
+      <a href="https://github.com/Mahennatadev" className="home__social-icon" target="_blank" rel="noopener noreferrer">
         <i className="uil uil-github-alt"></i>
       </a>
 
-      <a href="https://www.linkedin.com/" className="home__social-icon" target="_blank">
-      <i className="uil uil-linkedin"></i>
+      <a href="https://www.instagram.com/mahennata/" className="home__social-icon" target="_blank" rel="noopener noreferrer">
+        <i className="uil uil-instagram"></i>
       </a>
     </div>
   );
