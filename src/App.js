4b3178f1d8730cf866727d05ae93795315a36ebe
Mahennatadev <121371000+Mahennatadev@users.noreply.github.com>
2024-06-29 23:50:14 +0700

    more-update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
-import Services from './components/service/Services';
+import Services from './components/services/Services';
+import Qualification from './components/qualification/Qualification';
 import Contact from './components/contact/Contact';
-import Portofolio from './components/portofolio/Portofolio';
 
 const App = () => {
   return (
@@ -19,7 +19,7 @@ const App = () => {
       <About />
       <Skills />
       <Services />
-      <Portofolio />
+      <Qualification />
       <Contact />
     </main>
     </>
